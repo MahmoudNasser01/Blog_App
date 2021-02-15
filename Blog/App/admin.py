ee4fb2ec4e96ed0690b7068f12cc2ce260f8984e
elMeniwy <55510819+elMeniwy@users.noreply.github.com>
2021-02-16 01:03:56 +0200

    Basic Model

--- Blog/App/admin.py
+++ Blog/App/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from .models import Post, Comment
 
 admin.site.register(Post)
 
 admin.site.register(Comment)
-=======
 
-# Register your models here.
->>>>>>> 6865b5136ae409a9199d7719134fe4a019764f9b
